42221ca45223fe10b3a57a0fafa2e13a47e85e7e
unknown <szpunar@LAB6-10.users.coe.utah.edu>
2013-01-21 16:53:03 -0700

    Mobile rest service is now funcitonal, Stubs for functions are included, but there is limited functionality in the functions.

--- KServerTest/KServer/IMobile.cs
+++ KServerTest/KServer/IMobile.cs
@@ -13,14 +13,59 @@ namespace KServer
     public interface IMobile
     {
         [OperationContract]
-        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/{strSessionString}/activityStatus")]
-        MResponse test(string strSessionString);
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/test/?string={s}")]
+        Response test(string s);
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/MobileSignUp/?username={username}&password={password}")]
+        Response MobileSignUp(string username, string password);
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/MobileSignIn/?username={username}&password={password}")]
+        LogInResponse MobileSignIn(string username, string password);
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/MobileSignOut/?userKey={userKey}")]
+        Response MobileSignOut(long userKey);
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/MobileSongSearch/?title={title}&artist={artist}&venueID={venueID}")]
+        List<Song> MobileSongSearch(string title, string artist, int venueID);
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/MobileSongBrowse/?firstLetter={firstLetter}&isArtist={isArtist}&start={start}&count={count}&venueID={venueID}")]
+        List<Song> MobileSongBrowse(char firstLetter, bool isArtist, int start, int count, int venueID);
+
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/MobileSongRequest/?songID={songID}&userKey={userKey}")]
+        Response MobileSongRequest(int songID, long userKey);
+
+
     }
 
-    public class MResponse
-    {
-        public bool successful { get; set; }
-        public string comment { get; set; }
-    }
+
+    /*
+     *         [OperationContract]
+        // Song management
+        [OperationContract]
+        Response DJAddSongs(List<Song> songs, long DJKey);
+        [OperationContract]
+        Response DJRemoveSongs(List<Song> songs, long DJKey);
+        [OperationContract]
+        Response DJListSongs(out List<Song> songs, long DJKey);
+
+        // Queue management
+        [OperationContract]
+        Response DJAddQueue(SongRequest sr, int queueIndex, int sessionID, long DJKey);
+        [OperationContract]
+        Response DJRemoveSongRequest(SongRequest sr, int sessionID, long DJKey);
+        [OperationContract]
+        Response DJChangeSongRequest(SongRequest newSR, SongRequest oldSR, int sessionID, long DJKey);
+        [OperationContract]
+        Response DJRemoveUser(SongRequest newSR, SongRequest oldSR, int sessionID, long DJKey);
+        [OperationContract]
+        Response DJMoveUser(int userID, int sessionID, long DJKey);
+     */
+    
 
 }
--- KServerTest/KServer/Mobile.svc.cs
+++ KServerTest/KServer/Mobile.svc.cs
@@ -14,11 +14,69 @@ namespace KServer
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Multiple, IncludeExceptionDetailInFaults = true, AddressFilterMode = AddressFilterMode.Any)]
     public class Mobile : IMobile
     {
-        public MResponse test(string strSessionString)
+        public Response test(string s)
         {
-            MResponse r = new MResponse();
-            r.successful = true;
-            r.comment = strSessionString + "XYZ";
+            Response r = new Response();
+            for (int i = s.Length - 1; i >= 0; i--)
+                r.message += s[i];
+            r.result = s.Length;
+            return r;
+        }
+        public Response MobileSignUp(string username, string password)
+        {
+            Response r = new Response();
+            r.error = true;
+            r.message = "MobileSignUp is not yet implemented.";
+            return r;
+        }
+        public LogInResponse MobileSignIn(string username, string password)
+        {
+            LogInResponse r = new LogInResponse();
+            r.error = true;
+            r.message = "MobileSignIn is not yet implemented.";
+            r.result = -1;
+            r.userKey = 1337;
+            User u = new User();
+            u.userID = 3;
+            u.userName = username;
+            r.user = u;
+            return r;
+        }
+        public Response MobileSignOut(long userKey)
+        {
+            Response r = new Response();
+            r.error = true;
+            r.message = "MobileSignOut is not yet implemented.";
+            r.result = (int)userKey;
+            return r;
+        }
+
+        public List<Song> MobileSongSearch(string title, string artist, int venueID)
+        {
+            List<Song> s = new List<Song>();
+            return s;
+        }
+        public List<Song> MobileSongBrowse(char firstLetter, bool isArtist, int start, int count, int venueID)
+        {
+            List<Song> s = new List<Song>();
+            Song song = new Song();
+            song.artist = "Glitch Mob";
+            song.title = "Fortune Days";
+            song.pathOnDisk = "C:\\Songs\\Fortune Days";
+            s.Add(song);
+            Song song2 = new Song();
+            song.artist = "Rammstein";
+            song.title = "Du Hast";
+            song.pathOnDisk = "C:\\Songs\\Du Hast";
+            s.Add(song2);
+            return s;
+        }
+        public Response MobileSongRequest(int songID, long userKey)
+        {
+            Response r = new Response();
+            r.error = true;
+            r.message = "MobileSongRequest is not yet implemented.";
+            r.result = songID;
             return r;
         }
     }
