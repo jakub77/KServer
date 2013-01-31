e26db6cc97614798e39c8891c2a64b5670233a9f
Jakub Szpunar <jakub77@gmail.com>
2013-01-30 17:23:46 -0700

    A mobile test client now exists that can call any mobile server methods. Recommend for reference as to how to call methods etc.

--- KServer/JakubMobileClient/Program.cs
+++ KServer/JakubMobileClient/Program.cs
@@ -22,6 +22,7 @@ namespace JakubMobileClient
             string baseAddress = "http://sunny.eng.utah.edu:81/Mobile.svc";
             long userKey = -1;
             bool displayJSON = true;
+            int venueID = 1;
 
             for (; ; )
             {
@@ -31,15 +32,35 @@ namespace JakubMobileClient
                 if (command.StartsWith("h")) //HELP
                 {
                     //Console.WriteLine("help<h>, quit<q>, insertDJ<id>, signinDJ<si>, signoutDJ <so>, \nlistDJS<ld>, addSong<as>, removeSong<rs>, listSongs<ls>, \nlistQueue<lq>, popQueue<pq>");
-                    Console.WriteLine("help<h>, JSONOut<j>, quit<q>, signIn<si>, signOut<so>");
+                    Console.WriteLine("help<h>, JSONOut<j>, quit<q>, signUp<su>, signIn<si>, signOut<so>, songSearch<ss>,\nsongBrowse<sb>, songRequest<sr>, listQueue<lq>");
                 }
-                else if (command.StartsWith("j")) // QUIT
+                else if (command.StartsWith("j")) // TOGGLE JSON/OBJECT OUTPUT
                 {
                     displayJSON = !displayJSON;
                 }
                 else if (command.StartsWith("q")) // QUIT
                 {
                     return;
+                }
+                else if (command.StartsWith("su")) // SIGN UP
+                {
+                    string username, password;
+                    Console.Write("Username: ");
+                    username = Console.ReadLine();
+                    Console.Write("Password: ");
+                    password = Console.ReadLine();
+                    Stream data = client.OpenRead(baseAddress + "/MobileSignUp/?username=" + username.Trim() + "&password=" + password.Trim());
+                    StreamReader reader = new StreamReader(data);
+                    string s = reader.ReadToEnd();
+                    Response r = strToJSON<Response>(s);
+                    if (displayJSON)
+                        Console.WriteLine("JSON: " + s);
+                    else
+                    {
+                        Console.WriteLine("Error: " + r.error);
+                        Console.WriteLine("Result: " + r.result);
+                        Console.WriteLine("Message:\n" + r.message);
+                    }
                 }
                 else if (command.StartsWith("si"))
                 {
@@ -61,8 +82,7 @@ namespace JakubMobileClient
                         Console.WriteLine("Key: " + r.userKey);
                         Console.WriteLine("Message:\n" + r.message);
                     }
-                    userKey = r.userKey;
-                    
+                    userKey = r.userKey; 
                 }
                 else if (command.StartsWith("so")) // Signout a DJ
                 {
@@ -86,9 +106,133 @@ namespace JakubMobileClient
                         Console.WriteLine("Exception: " + e.Message);
                     }
                 }
+                else if (command.StartsWith("ss")) // Song Search
+                {
+                    try
+                    {
+                        string title, artist;
+                        Console.Write("Title: ");
+                        title = Console.ReadLine();
+                        Console.Write("Artist: ");
+                        artist = Console.ReadLine();
+                        Stream data = client.OpenRead(baseAddress + "/MobileSongSearch/?title=" + title.Trim() + "&artist=" + artist.Trim() + "&venueID=" + venueID);
+                        StreamReader reader = new StreamReader(data);
+                        string s = reader.ReadToEnd();
+
+                        if(s == "")
+                        {
+                            Console.WriteLine("An error occured");
+                            continue;
+                        }
+
+                        List<Song> r = strToJSON<List<Song>>(s);
+                        if (displayJSON)
+                            Console.WriteLine("JSON: " + s);
+                        else
+                        {
+                            foreach (Song song in r)
+                            {
+                                Console.WriteLine(song.ID + ", " + song.title + ", " + song.artist);
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Exception: " + e.Message);
+                    }
+                }
+                else if (command.StartsWith("sb")) // Song browse
+                {
+                    try
+                    {
+                        string firstLetter, isArtist, start, count;
+                        Console.Write("Title: ");
+                        firstLetter = Console.ReadLine();
+                        Console.Write("IsArtist (true/false): ");
+                        isArtist = Console.ReadLine();
+                        Console.Write("start: ");
+                        start = Console.ReadLine();
+                        Console.Write("count: ");
+                        count = Console.ReadLine();
+                        Stream data = client.OpenRead(baseAddress + "/MobileSongBrowse/?firstLetter=" + firstLetter + "&isArtist=" + isArtist + "&start=" + start + "&count=" + count + "&venueID=" + venueID);
+                        StreamReader reader = new StreamReader(data);
+                        string s = reader.ReadToEnd();
+
+                        if (s == "")
+                        {
+                            Console.WriteLine("An error occured");
+                            continue;
+                        }
+
+                        List<Song> r = strToJSON<List<Song>>(s);
+                        if (displayJSON)
+                            Console.WriteLine("JSON: " + s);
+                        else
+                        {
+                            foreach (Song song in r)
+                            {
+                                Console.WriteLine(song.ID + ", " + song.title + ", " + song.artist);
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Exception: " + e.Message);
+                    }
+                }
+                else if (command.StartsWith("sr")) // Song request
+                {
+                    try
+                    {
+                        string songID;
+                        Console.Write("SongID: ");
+                        songID = Console.ReadLine();
+                        Stream data = client.OpenRead(baseAddress + "/MobileSongRequest/?songID=" + songID + "&userKey=" + userKey);
+                        StreamReader reader = new StreamReader(data);
+                        string s = reader.ReadToEnd();
+                        Response r = strToJSON<Response>(s);
+                        if (displayJSON)
+                            Console.WriteLine("JSON: " + s);
+                        else
+                        {
+                            Console.WriteLine("Error: " + r.error);
+                            Console.WriteLine("Result: " + r.result);
+                            Console.WriteLine("Message:\n" + r.message);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Exception: " + e.Message);
+                    }
+                }
+                else if (command.StartsWith("lq")) // List the Queue
+                {
+                    try
+                    {
+                        Stream data = client.OpenRead(baseAddress + "/MobileViewQueue/?userKey=" + userKey);
+                        StreamReader reader = new StreamReader(data);
+                        string s = reader.ReadToEnd();
+                        List<queueSinger> queue = strToJSON<List<queueSinger>>(s);
+                        if (displayJSON)
+                            Console.WriteLine("JSON: " + s);
+                        else
+                        {
+                            foreach (queueSinger qs in queue)
+                            {
+                                Console.WriteLine("ID: " + qs.user.userID + ", Name: " + qs.user.userName);
+                                foreach (Song song in qs.songs)
+                                    Console.WriteLine("\t" + song.ID + ", " + song.title + ", " + song.artist);
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Exception: " + e.Message);
+                    }
+                }
 
 
-
+                //
             }
         }
 
